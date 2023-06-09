5693f0fc145fc0e0233b7a22d71eb2464330c23f
Shoaib Siddiqui <125572377+shoaib031@users.noreply.github.com>
2023-06-09 17:10:22 +0500

    Add files via upload

---
+++ javascript class1/app.js
@@ -0,0 +1,85 @@
+// // var num = 13;
+// // var anotherNum = 9;
+// // var number = num + anotherNum;
+// // alert(number)
+
+// var num = 13;
+// var anotherNum = 9;
+// var number = num - anotherNum;
+// alert(number)
+
+// var num = 13;
+// var anotherNu0m = 9;
+// var number = num % anotherNum;
+// alert(number)
+
+// var num = 13;
+// var anotherNum = 9;
+// var number = num * anotherNum;
+// alert(number)
+
+
+// var num = 13;
+// var anotherNum = 9;
+// var number = num / anotherNum;
+// alert(number)
+
+// var firstName = "shoaib"
+// var space = " "
+// var lastName = "siddiqui"
+// var fullName = firstName + space + lastName;
+// alert(fullName); 
+
+// var orignalNum = 50 + 50;
+// alert(orignalNum) 
+
+//  var name = "value after variable"
+//  var calculate = "value after variabe" + " ";
+//  alert(calculate)
+
+ // // var num = 13;
+// // var anotherNum = 9;
+// // var number = num + anotherNum;
+// // alert(number)
+
+// var num = 13;
+// var anotherNum = 9;
+// var number = num - anotherNum;
+// alert(number)
+
+// var num = 13;
+// var anotherNu0m = 9;
+// var number = num % anotherNum;
+// alert(number)
+
+// var num = 13;
+// var anotherNum = 9;
+// var number = num * anotherNum;
+// alert(number)
+
+
+// var num = 13;
+// var anotherNum = 9;
+// var number = num / anotherNum;
+// alert(number)
+
+// var firstName = "shoaib"
+// var space = " "
+// var lastName = "siddiqui"
+// var fullName = firstName + space + lastName;
+// alert(fullName); 
+
+// var orignalNum = 50 + 50;
+// alert(orignalNum) 
+
+var centigtadeTemp = 12;
+var farenTemp = 68;
+
+var centitofaren = (farenTemp - 32) * 5 / 9;
+var farentocenti = (centigtadeTemp * 9 / 5) + 32;
+
+alert(farentocenti);
+alert(centitofaren);
+
+
+
